0740940fcc9a2e46b6ae679abe991de2de96e7db
Muhammad Majid <mmajidkamel@gmail.com>
2021-12-12 13:09:52 +0100

    Init, append, and loop over a slice

--- 2.cards/Notes.md
+++ 2.cards/Notes.md
@@ -13,3 +13,7 @@ func main() {
 }
 
 ```
+
+* GO has two types of lists, namely, `Array` (Fixed length list of things), and `Slice` (An array that can grow or shrink)
+* Elements' datatype in the array or slice should be the same.
+* Note `sliceVar = append(sliceVar, "newElement")` will create a new slice and assign it to `sliceVar`, instead of modifying the old `sliceVar` slice.
--- 2.cards/main.go
+++ 2.cards/main.go
@@ -4,7 +4,24 @@ import "fmt"
 
 func main() {
 	var card string = "Ace of Spades"
+	anotherCard := newCard()
 	// Another way is to rely on Go compiler and infer the datatype from the assigned value
 	// card := "Ace of Spades"
+
+	// Slice
+	cards := []string{newCard(), newCard(), card, "Queen of Hearts"}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
 	fmt.Println(card)
+	fmt.Println(anotherCard)
+	fmt.Println(cards)
+}
+
+// string here represents function's return type
+func newCard() string {
+	return "Five of Diamonds"
 }
