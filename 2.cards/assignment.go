b77b8290467daf24e237da910b1614a9b7a25f4d
Muhammad Majid <mmajidkamel@gmail.com>
2021-12-12 20:30:32 +0100

    Create assignment.go

---
+++ 2.cards/assignment.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main2() {
+	nums := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, v := range nums {
+		if v%2 == 0 {
+			fmt.Println(string(v) + " is even")
+		} else {
+			fmt.Println(string(v) + " is odd")
+		}
+	}
+}
