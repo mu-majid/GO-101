36f574aaafac75c2a9f3f3619e20d0c588ba162d
Muhammad Majid <mmajidkamel@gmail.com>
2021-12-12 14:40:51 +0100

    Added newDeck and deal methods to deck type

--- 2.cards/deck.go
+++ 2.cards/deck.go
@@ -8,8 +8,27 @@ import "fmt"
 // this line means deck extends (inherits) all functions/methods from 'Slice' type
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardsSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "King", "Queen", "Jack", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"}
+
+	for _, s := range cardsSuits {
+		for _, v := range cardValues {
+			cards = append(cards, s+" Of "+v)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
--- 2.cards/main.go
+++ 2.cards/main.go
@@ -3,22 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
-	anotherCard := newCard()
-	// Another way is to rely on Go compiler and infer the datatype from the assigned value
-	// card := "Ace of Spades"
+	cards := newDeck()
+	hand, remainingCards := deal(cards, 5)
 
-	// Slice
-	cards := deck{newCard(), newCard(), card, "Queen of Hearts"}
-	cards = append(cards, "Six of Spades")
-
-	cards.print()
-
-	fmt.Println(card)
-	fmt.Println(anotherCard)
-}
-
-// string here represents function's return type
-func newCard() string {
-	return "Five of Diamonds"
+	hand.print()
+	fmt.Println("--------")
+	remainingCards.print()
 }
