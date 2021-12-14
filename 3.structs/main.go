ea3684d69f55666dfdc2ab49966b108e3b870051
Muhammad Majid <mmajidkamel@gmail.com>
2021-12-14 20:39:11 +0100

    struct in a struct

--- 3.structs/main.go
+++ 3.structs/main.go
@@ -5,15 +5,24 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
 }
 
 func main() {
 
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contact:   contactInfo{email: "email", zipCode: 11122}}
 
 	fmt.Println(alex)
 
-	marc := person{"Marc", "Bill"}
+	marc := person{"Marc", "Bill", contactInfo{email: "email", zipCode: 11122}}
 
 	fmt.Println(marc)
 
